579233eb64ff6bf58eb7f89aa4744935d3377296
VolodymyrHorodetskyy <horodetskyyv@gmail.com>
2017-04-15 22:54:10 +0300

    search by name implemented

--- src/main/java/ua/vhor/repository/ProductRepository.java
+++ src/main/java/ua/vhor/repository/ProductRepository.java
@@ -12,7 +12,6 @@ import ua.vhor.db.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
 
-	static final String GET_PRODUCTS_BY_PRICE_AND_CATEGORY = "SELECT * FROM es_product ep where ep.es_pdct_available=1 AND ep.es_pdct_price BETWEEN  COALESCE(:minPrice, 0) AND COALESCE(:maxPrice, (select max(es_pdct_price) from exhibit.es_product ep1 where es_pdct_available =1)) and ep.es_pdct_category = COALESCE(NULLIF(:categoryId, ''), ep.es_pdct_category) LIMIT :page, :quantityofcard";
 	static final String GET_BIGGEST_PRICE_QUERY_BY_NAME_AND_CATEGORY = "SELECT MAX(ep.es_pdct_price) FROM es_product ep WHERE ep.es_pdct_available=1 AND ep.es_pdct_category = COALESCE(NULLIF(:category, ''), ep.es_pdct_category) AND ep.es_pdct_name LIKE CONCAT('%', COALESCE(NULLIF(:name, ''), ep.es_pdct_name), '%');";
 	static final String GET_LEAST_PRICE_QUERY_BY_NAME_AND_CATEGORY = "SELECT MIN(ep.es_pdct_price) FROM es_product ep WHERE ep.es_pdct_available=1 AND ep.es_pdct_category = COALESCE(NULLIF(:category, ''), ep.es_pdct_category) AND ep.es_pdct_name LIKE CONCAT('%', COALESCE(NULLIF(:name, ''), ep.es_pdct_name), '%');";
 
@@ -21,9 +20,9 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 	@Query(value = GET_PRODUCTS_BY_PRICE_AND_CATEGORY, nativeQuery = true)
 	public List<Product> findByPriceBetweenAndCategoryId(
 			@Param("minPrice") Double min, @Param("maxPrice") Double max,
-			@Param("categoryId") Integer categorId,
+			@Param("categoryId") Integer categorId, @Param("name") String name,
 			@Param("page") Integer page,
-			@Param("quantityofcard") Integer quantityOfCards);
+			@Param("quantityofcards") Integer quantityOfCards);
 
 	Page<Product> findByPriceBetween(double min, double max, Pageable pageable);
 
